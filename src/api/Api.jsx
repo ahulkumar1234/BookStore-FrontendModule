274f4d8f93c52fef51f5d8ed76e9c67f9b13f885
Rahul Kumar <rahulkumar8340527941@gmail.com>
2026-02-16 15:50:20 +0530

    api update

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:9000/api/v1",
+  baseURL: "https://cuvette-backend-module.onrender.com/api/v1",
   withCredentials: true,
 });
